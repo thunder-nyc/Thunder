74204d3dbdb90cca81ab51260edc26e9fc9cc1da
Xiang Zhang <zhangxiangxiao@163.com>
2014-08-08 15:41:32 -0400

    Removing doxygen style license for tests

--- test/storage.cpp
+++ test/storage.cpp
@@ -1,6 +1,5 @@
 /*
- * \copyright Copyright 2014 Xiang Zhang. All Rights Reserved.
- * \license @{
+ * Copyright 2014 Xiang Zhang. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * @}
  */
 
 #include "thunder/storage.hpp"
