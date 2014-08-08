93420cff467f4539cd6ec28b615cb57433489d0f
Xiang Zhang <zhangxiangxiao@163.com>
2014-08-07 23:54:45 -0700

    Test on thunder::Storage<T, A> completed

--- test/storage.cpp
+++ test/storage.cpp
@@ -61,10 +61,10 @@ void ConstructorTest() {
   EXPECT_NE(nullptr, size_storage.Data());
 
   // Construct an storage with some size and a default value
-  thunder::Storage<T> size_value_storage(5,(T)3);
+  thunder::Storage<T> size_value_storage(5, (T)3);
   EXPECT_EQ(5, size_storage.Size());
   EXPECT_NE(nullptr, size_value_storage.Data());
-  for(int i = 0; i < 5; ++i) {
+  for (int i = 0; i < 5; ++i) {
     EXPECT_EQ(3, size_value_storage.Data()[i]);
   }
 
@@ -72,7 +72,7 @@ void ConstructorTest() {
   thunder::Storage<T> copy_storage(size_storage);
   EXPECT_EQ(5, copy_storage.Size());
   EXPECT_NE(nullptr, copy_storage.Data());
-  for(int i = 0; i < 5; ++i) {
+  for (int i = 0; i < 5; ++i) {
     EXPECT_EQ(size_storage.Data()[i], copy_storage.Data()[i]);
   }
 }
@@ -82,7 +82,7 @@ template <typename T>
 void AssignmentTest() {
   // Create a storage of size 5 and assign values to it
   thunder::Storage<T> storage(5);
-  for(int i = 0; i < 5; ++i) {
+  for (int i = 0; i < 5; ++i) {
     storage.Data()[i] = static_cast<T>(i + 3);
   }
 
@@ -90,14 +90,14 @@ void AssignmentTest() {
   thunder::Storage<T> assign_storage(10);
   assign_storage = storage;
   EXPECT_EQ(5, assign_storage.Size());
-  for(int i = 0; i < 5; ++i) {
+  for (int i = 0; i < 5; ++i) {
     EXPECT_EQ(storage.Data()[i], assign_storage.Data()[i]);
   }
 
   // Assign to self
   assign_storage = assign_storage;
   EXPECT_EQ(5, assign_storage.Size());
-  for(int i = 0; i < 5; ++i) {
+  for (int i = 0; i < 5; ++i) {
     EXPECT_EQ(storage.Data()[i], assign_storage.Data()[i]);
   }
 }
@@ -107,12 +107,12 @@ template <typename T>
 void IndexAndIteratorTest() {
   // Create a storage of size 5 and assign values to it
   thunder::Storage<T> storage(5);
-  for(int i = 0; i < 5; ++i) {
+  for (int i = 0; i < 5; ++i) {
     storage[i] = static_cast<T>(i + 3);
     EXPECT_EQ(static_cast<T>(i + 3), storage[i]);
   }
   int i = 0;
-  for(const T& val : storage) {
+  for (const T& val : storage) {
     EXPECT_EQ(static_cast<T>(i + 3), val);
     ++i;
   }
@@ -123,20 +123,20 @@ template <typename T>
 void CopyTest() {
   // Create an int storage of size 5 and assign values to it
   thunder::Storage<int> double_storage(5);
-  for(int i = 0; i < 5; ++i) {
+  for (int i = 0; i < 5; ++i) {
     double_storage[i] = static_cast<double>(i + 3);
   }
 
   // Copy to self
   double_storage.Copy(double_storage);
-  for(int i = 0; i < 5; ++i) {
+  for (int i = 0; i < 5; ++i) {
     EXPECT_EQ(static_cast<double>(i + 3), double_storage[i]);
   }
 
   // Create an empty storage of type T
   thunder::Storage<T> storage;
   storage.Copy(double_storage);
-  for(int i = 0; i < 5; ++i) {
+  for (int i = 0; i < 5; ++i) {
     EXPECT_EQ(static_cast<T>(i + 3), storage[i]);
   }
 }
@@ -149,7 +149,7 @@ void ResizeTest() {
 
   // Resize to 5 with default value
   storage.Resize(5, (T)3);
-  for(int i = 0; i < 5; ++i) {
+  for (int i = 0; i < 5; ++i) {
     EXPECT_EQ((T)3, storage[i]);
   }
 }
