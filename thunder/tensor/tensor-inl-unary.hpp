565a4a32ad970f693522524ccc04fc4a9c4180d8
Xiang Zhang <zhangxiangxiao@163.com>
2014-09-06 11:31:37 -0400

    Added unary mathematical operations implementation

--- thunder/tensor/tensor-inl-unary.hpp
+++ thunder/tensor/tensor-inl-unary.hpp
@@ -30,7 +30,82 @@
 namespace thunder {
 namespace tensor {
 
+#define THUNDER_TENSOR_DECLARE_UNARY(func)                              \
+  template < typename S >                                               \
+  const Tensor< S >& Tensor< S >::func() const {                        \
+    if (partialContiguity(0, size_.size() - 1)) {                       \
+      pointer data_pointer = data();                                    \
+      difference_type data_step = stride_[stride_.size() - 1];          \
+      size_type data_length = length();                                 \
+      for (size_type i = 0; i < data_length; ++i) {                     \
+        data_pointer[i * step] = ::std::func(data_pointer[i * step]);   \
+      }                                                                 \
+    } else {                                                            \
+      for (reference_iterator begin = reference_begin(),                \
+               end = reference_end(); begin != end; ++begin) {          \
+        *begin = ::std::func(*begin);                                   \
+      }                                                                 \
+    }                                                                   \
+    return *this;                                                       \
+  }                                                                     \
+  template < typename S >                                               \
+  Tensor< S >& Tensor< S >::func() {                                    \
+    return const_cast< Tensor& >(                                       \
+        const_cast< const Tensor* >(this)->func());                     \
+  }                                                                     \
+  template < typename S >                                               \
+  Tensor< S > Tensor< S >::func(const Tensor &x) {                      \
+    return x.clone().func();                                            \
+  }
 
+THUNDER_TENSOR_DECLARE_UNARY(abs);
+THUNDER_TENSOR_DECLARE_UNARY(exp);
+THUNDER_TENSOR_DECLARE_UNARY(exp2);
+THUNDER_TENSOR_DECLARE_UNARY(expm1);
+THUNDER_TENSOR_DECLARE_UNARY(log);
+THUNDER_TENSOR_DECLARE_UNARY(log10);
+THUNDER_TENSOR_DECLARE_UNARY(log2);
+THUNDER_TENSOR_DECLARE_UNARY(log1p);
+THUNDER_TENSOR_DECLARE_UNARY(sqrt);
+THUNDER_TENSOR_DECLARE_UNARY(cbrt);
+THUNDER_TENSOR_DECLARE_UNARY(sin);
+THUNDER_TENSOR_DECLARE_UNARY(cos);
+THUNDER_TENSOR_DECLARE_UNARY(tan);
+THUNDER_TENSOR_DECLARE_UNARY(asin);
+THUNDER_TENSOR_DECLARE_UNARY(acos);
+THUNDER_TENSOR_DECLARE_UNARY(atan);
+THUNDER_TENSOR_DECLARE_UNARY(sinh);
+THUNDER_TENSOR_DECLARE_UNARY(cosh);
+THUNDER_TENSOR_DECLARE_UNARY(tanh);
+THUNDER_TENSOR_DECLARE_UNARY(asinh);
+THUNDER_TENSOR_DECLARE_UNARY(acosh);
+THUNDER_TENSOR_DECLARE_UNARY(atanh);
+THUNDER_TENSOR_DECLARE_UNARY(erf);
+THUNDER_TENSOR_DECLARE_UNARY(erfc);
+THUNDER_TENSOR_DECLARE_UNARY(tgamma);
+THUNDER_TENSOR_DECLARE_UNARY(lgamma);
+THUNDER_TENSOR_DECLARE_UNARY(ceil);
+THUNDER_TENSOR_DECLARE_UNARY(floor);
+THUNDER_TENSOR_DECLARE_UNARY(trunc);
+THUNDER_TENSOR_DECLARE_UNARY(round);
+THUNDER_TENSOR_DECLARE_UNARY(nearbyint);
+THUNDER_TENSOR_DECLARE_UNARY(rint);
+THUNDER_TENSOR_DECLARE_UNARY(logb);
+THUNDER_TENSOR_DECLARE_UNARY(fpclassify);
+THUNDER_TENSOR_DECLARE_UNARY(isfinite);
+THUNDER_TENSOR_DECLARE_UNARY(isinf);
+THUNDER_TENSOR_DECLARE_UNARY(isnan);
+THUNDER_TENSOR_DECLARE_UNARY(isnormal);
+THUNDER_TENSOR_DECLARE_UNARY(signbit);
+THUNDER_TENSOR_DECLARE_UNARY(zero);
+THUNDER_TENSOR_DECLARE_UNARY(real);
+THUNDER_TENSOR_DECLARE_UNARY(imag);
+THUNDER_TENSOR_DECLARE_UNARY(arg);
+THUNDER_TENSOR_DECLARE_UNARY(cnrm);
+THUNDER_TENSOR_DECLARE_UNARY(conj);
+THUNDER_TENSOR_DECLARE_UNARY(proj);
+
+#undef THUNDER_TENSOR_DECLARE_UNARY
 
 }  // namespace tensor
 }  // namespace thunder
