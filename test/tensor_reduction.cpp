ade2cb058ae5aa8e4812b5613bd33e0212497af3
Xiang Zhang <zhangxiangxiao@163.com>
2014-09-12 15:22:20 -0400

    Implemented and tested prod reduction

--- test/tensor_reduction.cpp
+++ test/tensor_reduction.cpp
@@ -185,5 +185,102 @@ TEST(TensorTest, minTest) {
   minTest< Tensor< Storage< int > > >();
 }
 
+template < typename T >
+void sumTest() {
+  T t1(10, 20, 7);
+  int t1_val = 0;
+  for (typename T::reference_iterator begin = t1.reference_begin(),
+           end = t1.reference_end(); begin != end; ++begin) {
+    *begin = static_cast< typename T::value_type >(t1_val++);
+  }
+
+  T t1_result = T::sum(t1, 1);
+  EXPECT_EQ(3, t1_result.dimension());
+  EXPECT_EQ(10, t1_result.size(0));
+  EXPECT_EQ(1, t1_result.size(1));
+  EXPECT_EQ(7, t1_result.size(2));
+  for (int i = 0; i < 10; ++i) {
+    for (int j = 0; j < 7; ++j) {
+      EXPECT_EQ(t1[i].select(1, j).sum(), t1_result(i, 0, j));
+    }
+  }
+
+  T t2({10, 20, 7, 9}, {1431, 71, 10, 1});
+  int t2_val = 0;
+  for (typename T::reference_iterator begin = t2.reference_begin(),
+           end = t2.reference_end(); begin != end; ++begin) {
+    *begin = static_cast< typename T::value_type >(t2_val++);
+  }
+
+  T t2_result = T::sum(t2, 1);
+  EXPECT_EQ(4, t2_result.dimension());
+  EXPECT_EQ(10, t2_result.size(0));
+  EXPECT_EQ(1, t2_result.size(1));
+  EXPECT_EQ(7, t2_result.size(2));
+  EXPECT_EQ(9, t2_result.size(3));
+  for (int i = 0; i < 10; ++i) {
+    for (int j = 0; j < 7; ++j) {
+      for (int k = 0; k < 9; ++k) {
+        EXPECT_EQ(t2[i].select(1, j).select(1, k).sum(), t2_result(i, 0, j, k));
+      }
+    }
+  }
+}
+
+TEST(TensorTest, sumTest) {
+  sumTest< DoubleTensor >();
+  sumTest< FloatTensor >();
+  sumTest< Tensor< Storage< int > > >();
+}
+
+template < typename T >
+void prodTest() {
+  T t1(10, 20, 7);
+  int t1_val = 0;
+  for (typename T::reference_iterator begin = t1.reference_begin(),
+           end = t1.reference_end(); begin != end; ++begin) {
+    *begin = static_cast< typename T::value_type >(t1_val++);
+  }
+
+  T t1_result = T::prod(t1, 1);
+  EXPECT_EQ(3, t1_result.dimension());
+  EXPECT_EQ(10, t1_result.size(0));
+  EXPECT_EQ(1, t1_result.size(1));
+  EXPECT_EQ(7, t1_result.size(2));
+  for (int i = 0; i < 10; ++i) {
+    for (int j = 0; j < 7; ++j) {
+      EXPECT_EQ(t1[i].select(1, j).prod(), t1_result(i, 0, j));
+    }
+  }
+
+  T t2({10, 20, 7, 9}, {1431, 71, 10, 1});
+  int t2_val = 0;
+  for (typename T::reference_iterator begin = t2.reference_begin(),
+           end = t2.reference_end(); begin != end; ++begin) {
+    *begin = static_cast< typename T::value_type >(t2_val++);
+  }
+
+  T t2_result = T::prod(t2, 1);
+  EXPECT_EQ(4, t2_result.dimension());
+  EXPECT_EQ(10, t2_result.size(0));
+  EXPECT_EQ(1, t2_result.size(1));
+  EXPECT_EQ(7, t2_result.size(2));
+  EXPECT_EQ(9, t2_result.size(3));
+  for (int i = 0; i < 10; ++i) {
+    for (int j = 0; j < 7; ++j) {
+      for (int k = 0; k < 9; ++k) {
+        EXPECT_EQ(t2[i].select(1, j).select(1, k).prod(),
+                  t2_result(i, 0, j, k));
+      }
+    }
+  }
+}
+
+TEST(TensorTest, prodTest) {
+  prodTest< DoubleTensor >();
+  prodTest< FloatTensor >();
+  prodTest< Tensor< Storage< int > > >();
+}
+
 }  // namespace
 }  // namespace thunder
--- thunder/tensor/tensor-inl-reduction.hpp
+++ thunder/tensor/tensor-inl-reduction.hpp
@@ -412,12 +412,116 @@ Tensor< S > Tensor< S >::min(dim_type d) const {
 
 template < typename S >
 Tensor< S > Tensor< S >::sum(dim_type d) const {
-  return Tensor();
+  if (d >= size_.size()) {
+    throw out_of_range("Dimension exceeds limit");
+  }
+  size_storage sz = size_;
+  sz[d] = 1;
+  Tensor t(sz);
+  if (partialContiguity(0, d) && partialContiguity(d + 1, size_.size() - 1)) {
+    // Get data pointers
+    pointer x_data = data();
+    pointer t_data = t.data();
+    // Determine left and right size
+    size_type x_left_length = 1;
+    for (dim_type i = 0; i < d; ++i) {
+      x_left_length *= size_[i];
+    }
+    size_type x_length = size_[d];
+    size_type x_right_length = 1;
+    for (dim_type i = d + 1; i < size_.size(); ++i) {
+      x_right_length *= size_[i];
+    }
+    // Determine left and right step
+    difference_type x_left_step = d > 0 ? stride_[d - 1] : 0;
+    difference_type x_step = stride_[d];
+    difference_type x_right_step = stride_[stride_.size() - 1];
+    difference_type t_left_step = d > 0 ? t.stride(d - 1) : 0;
+    difference_type t_right_step = t.stride(t.dimension() - 1);
+    // Run the loop
+    value_type sum_value = 0;
+    for (size_type i = 0; i < x_left_length; ++i) {
+      for (size_type j = 0; j < x_right_length; ++j) {
+        sum_value = 0;
+        for (size_type k = 0; k < x_length; ++k) {
+          sum_value += x_data[i * x_left_step + j * x_right_step + k * x_step];
+        }
+        t_data[i * t_left_step + j * t_right_step] = sum_value;
+      }
+    }
+  } else {
+    size_storage position;
+    size_type size_d = size_[d];
+    value_type sum_value = 0;
+    for (reference_iterator begin = t.reference_begin(),
+             end = t.reference_end(); begin != end; ++begin) {
+      sum_value = 0;
+      position = begin.position();
+      for (size_type i = 0; i < size_d; ++i) {
+        position[d] = i;
+        sum_value += (*this)(position);
+      }
+      *begin = sum_value;
+    }
+  }
+  return t;
 }
 
 template < typename S >
 Tensor< S > Tensor< S >::prod(dim_type d) const {
-  return Tensor();
+  if (d >= size_.size()) {
+    throw out_of_range("Dimension exceeds limit");
+  }
+  size_storage sz = size_;
+  sz[d] = 1;
+  Tensor t(sz);
+  if (partialContiguity(0, d) && partialContiguity(d + 1, size_.size() - 1)) {
+    // Get data pointers
+    pointer x_data = data();
+    pointer t_data = t.data();
+    // Determine left and right size
+    size_type x_left_length = 1;
+    for (dim_type i = 0; i < d; ++i) {
+      x_left_length *= size_[i];
+    }
+    size_type x_length = size_[d];
+    size_type x_right_length = 1;
+    for (dim_type i = d + 1; i < size_.size(); ++i) {
+      x_right_length *= size_[i];
+    }
+    // Determine left and right step
+    difference_type x_left_step = d > 0 ? stride_[d - 1] : 0;
+    difference_type x_step = stride_[d];
+    difference_type x_right_step = stride_[stride_.size() - 1];
+    difference_type t_left_step = d > 0 ? t.stride(d - 1) : 0;
+    difference_type t_right_step = t.stride(t.dimension() - 1);
+    // Run the loop
+    value_type prod_value = 1;
+    for (size_type i = 0; i < x_left_length; ++i) {
+      for (size_type j = 0; j < x_right_length; ++j) {
+        prod_value = 1;
+        for (size_type k = 0; k < x_length; ++k) {
+          prod_value *= x_data[i * x_left_step + j * x_right_step + k * x_step];
+        }
+        t_data[i * t_left_step + j * t_right_step] = prod_value;
+      }
+    }
+  } else {
+    size_storage position;
+    size_type size_d = size_[d];
+    value_type prod_value = 1;
+    for (reference_iterator begin = t.reference_begin(),
+             end = t.reference_end(); begin != end; ++begin) {
+      prod_value = 1;
+      position = begin.position();
+      for (size_type i = 0; i < size_d; ++i) {
+        position[d] = i;
+        prod_value *= (*this)(position);
+      }
+      *begin = prod_value;
+    }
+  }
+  return t;
 }
 
 template < typename S >
