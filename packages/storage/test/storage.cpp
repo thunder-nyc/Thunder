993beca70b26bb31806f46f7b56ac892bf6d30a6
Xiang Zhang <zhangxiangxiao@163.com>
2014-10-20 09:09:06 -0400

    Changed license format in all tests

--- packages/storage/test/storage.cpp
+++ packages/storage/test/storage.cpp
@@ -1,5 +1,6 @@
 /*
- * Copyright 2014 Xiang Zhang. All Rights Reserved.
+ * \copyright Copyright 2014 Xiang Zhang All Rights Reserved.
+ * \license @{
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
+ * @}
  */
 
 #include "thunder/storage.hpp"
