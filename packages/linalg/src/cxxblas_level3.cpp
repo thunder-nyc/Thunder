51585e675f1b007419c583df8e378a9a80e326ca
Xiang Zhang <zhangxiangxiao@163.com>
2014-11-28 14:21:04 -0500

    Added CXXBLAS level 3 source file

--- packages/linalg/src/cxxblas_level3.cpp
+++ packages/linalg/src/cxxblas_level3.cpp
@@ -27,7 +27,85 @@ namespace thunder {
 namespace linalg {
 namespace cxxblas {
 
+static inline char transChar(Trans trans) {
+  if (trans == Trans::kNoTrans) {
+    return 'N';
+  } else if (trans == Trans::kTrans) {
+    return 'T';
+  } else if (trans == Trans::kConjTrans) {
+    return 'C';
+  }
+  return '\0';
+}
 
+static inline char uploChar(Order order, Uplo uplo) {
+  if (order == Order::kColMajor) {
+    if (uplo == Uplo::kUpper) {
+      return 'U';
+    } else if (uplo == Uplo::kLower) {
+      return 'L';
+    }
+  } else if (order == Order::kRowMajor) {
+    if (uplo == Uplo::kUpper) {
+      return 'L';
+    } else if (uplo == Uplo::kLower) {
+      return 'U';
+    }
+  }
+  return '\0';
+}
+
+static inline char diagChar(Diag diag) {
+  if (diag == Diag::kNonUnit) {
+    return 'N';
+  } else if (diag == Diag::kUnit){
+    return 'U';
+  }
+  return '\0';
+}
+
+template <typename D>
+static inline void conj(int n, const D *x, int incx, D *y, int incy) {
+  for (int i = 0; i < n; ++i) {
+    y[i * incy] = ::std::conj(x[i * incx]);
+  }
+}
+
+void gemm(int m, int n, int k, const float *a, const float *b, float *c,
+          float alpha, float beta, int lda, int ldb, int ldc, Order order,
+          Trans transa, Trans transb) {
+  char transa_char = transChar(transa);
+  char transb_char = transChar(transb);
+}
+
+void gemm(int m, int n, int k, const double *a, const double *b, double *c,
+          double alpha, double beta, int lda, int ldb, int ldc, Order order,
+          Trans transa, Trans transb) {
+}
+
+void gemm(int m, int n, int k, const ::std::complex< float > *a,
+          const ::std::complex< float > *b, ::std::complex< float > *c,
+          ::std::complex< float > alpha, ::std::complex< float > beta, int lda,
+          int ldb, int ldc, Order order, Trans transa, Trans transb) {
+}
+
+void gemm(int m, int n, int k, const ::std::complex< double > *a,
+          const ::std::complex< double > *b, ::std::complex< double > *c,
+          ::std::complex< double > alpha, ::std::complex< double > beta,
+          int lda, int ldb, int ldc, Order order, Trans transa, Trans transb) {
+}
+
+void gemm(int m, int n, int k, const float *a, const ::std::complex< float > *b,
+          ::std::complex< float > *c, ::std::complex< float > alpha,
+          ::std::complex< float > beta, int lda, int ldb, int ldc, Order order,
+          Trans transa, Trans transb) {
+}
+
+void gemm(int m, int n, int k, const double *a,
+          const ::std::complex< double > *b, ::std::complex< double > *c,
+          ::std::complex< double > alpha, ::std::complex< double > beta,
+          int lda, int ldb, int ldc, Order order, Trans transa, Trans transb) {
+}
 
 }  // namespace cxxblas
 }  // namespace linalg
