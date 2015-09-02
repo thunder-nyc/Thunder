9521b638b402ac4335e8a63461ea5e16487d831e
Xiang Zhang <zhangxiangxiao@163.com>
2015-09-02 18:22:02 -0400

    Added tests for gbmv and gemv in CxxBlas

--- packages/linalg/test/cxxblas_level2.cpp
+++ packages/linalg/test/cxxblas_level2.cpp
@@ -17,6 +17,11 @@
  * @}
  */
 
+/* Disclaimer: these tests are not complete. In particular, we do not test the
+ * variants of order, transpose and uppper-lower choices. We always use
+ * row-major, no transpose and upper matrix settings.
+ */
+
 #include <complex>
 #include <cstdlib>
 #include <random>
@@ -45,28 +50,123 @@ void expectEq(
   EXPECT_FLOAT_EQ(::std::imag(a), ::std::imag(b));
 }
 
-TEST(CxxBlasTest, gbmvTest) {
+template < typename D >
+class RandomGenerator {
+ public:
+  template < typename G, typename R >
+  D operator()(G *gen, R *dist) {
+    return static_cast< D >((*dist)(*gen));
+  }
+};
+
+template < typename D >
+class RandomGenerator< ::std::complex< D > > {
+ public:
+  template < typename G, typename R >
+  ::std::complex< D > operator()(G *gen, R *dist) {
+    return ::std::complex< D >(
+        static_cast< D >((*dist)(*gen)), static_cast< D >((*dist)(*gen)));
+  }
+};
+
+template < typename D >
+void gbmvTest() {
   ::std::random_device rd;
   ::std::mt19937 gen(rd());
   ::std::uniform_real_distribution< double > dist(-1.0, 1.0);
-  
+  RandomGenerator< D > rand;
+
   const int m = 23;
   const int n = 40;
   const int kl = 3;
   const int ku = 2;
-  const int lda = 5;
+  const int lda = 8;
+  const int incx = 2;
+  const int incy = 3;
 
-  float sa[lda * n], sx[::std::max(m, n)], sy[::std::max(m, n)];
-  float salpha = static_cast< float >(dist(gen));
-  float sbeta = static_cast< float >(dist(gen));
-  for (int i = 0; i < lda * n; ++i) {
-    sa[i] = static_cast< float >(dist(gen));
+  D a[lda * m], x[n * incx], y[m * incy], y_orig[m * incy];
+  D alpha = rand(&gen, &dist);
+  D beta = rand(&gen, &dist);
+  for (int i = 0; i < lda * m; ++i) {
+    a[i] = rand(&gen, &dist);
   }
-  for (int i = 0; i < ::std::max(m, n); ++i) {
-    sx[i] = static_cast< float >(dist(gen));
-    sy[i] = static_cast< float >(dist(gen));
+  for (int i = 0; i < n; ++i) {
+    x[i * incx] = rand(&gen, &dist);
   }
-  cxxblas::gbmv(m, n, sa, sx, sy);
+  for (int i = 0; i < m; ++i) {
+    y[i * incy] = rand(&gen, &dist);
+    y_orig[i * incy] = y[i * incy];
+  }
+  cxxblas::gbmv(m, n, a, x, y, alpha, beta, kl, ku, lda, incx, incy);
+
+  D result = 0.0;
+  int x_index = 0;
+  for (int i = 0; i < m; ++i) {
+    result = 0.0;
+    for (int j = 0; j < kl + ku + 1; ++j) {
+      x_index = j - kl + i;
+      if (x_index >= 0 && x_index < n) {
+        result += a[lda * i + j] * x[x_index * incx];
+      }
+    }
+    result = alpha * result + beta * y_orig[i * incy];
+    expectEq(y[i * incy], result);
+  }
+}
+
+TEST(CxxBlasTest, gbmvTest) {
+  gbmvTest< double >();
+  gbmvTest< float >();
+  gbmvTest< ::std::complex< double > >();
+  gbmvTest< ::std::complex< float > >();
+}
+
+template < typename D >
+void gemvTest() {
+  ::std::random_device rd;
+  ::std::mt19937 gen(rd());
+  ::std::uniform_real_distribution< double > dist(-1.0, 1.0);
+  RandomGenerator< D > rand;
+
+  const int m = 23;
+  const int n = 40;
+  const int kl = 3;
+  const int ku = 2;
+  const int lda = 54;
+  const int incx = 2;
+  const int incy = 3;
+
+  D a[lda * m], x[n * incx], y[m * incy], y_orig[m * incy];
+  D alpha = rand(&gen, &dist);
+  D beta = rand(&gen, &dist);
+  for (int i = 0; i < lda * m; ++i) {
+    a[i] = rand(&gen, &dist);
+  }
+  for (int i = 0; i < n; ++i) {
+    x[i * incx] = rand(&gen, &dist);
+  }
+  for (int i = 0; i < m; ++i) {
+    y[i * incy] = rand(&gen, &dist);
+    y_orig[i * incy] = y[i * incy];
+  }
+  cxxblas::gemv(m, n, a, x, y, alpha, beta, lda, incx, incy);
+
+  D result = 0.0;
+  for (int i = 0; i < m; ++i) {
+    result = 0.0;
+    for (int j = 0; j < n; ++j) {
+      result += a[lda * i + j] * x[j * incx];
+    }
+    result = alpha * result + beta * y_orig[i * incy];
+    expectEq(y[i * incy], result);
+  }
+}
+
+TEST(CxxBlasTest, gemvTest) {
+  gemvTest< double >();
+  gemvTest< float >();
+  gemvTest< ::std::complex< double > >();
+  gemvTest< ::std::complex< float > >();
 }
 
 }  // namespace
